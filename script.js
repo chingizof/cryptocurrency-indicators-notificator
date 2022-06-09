a5a838d6c3906ba1cc5a4c5970b83eca0f36e415
Mister-718 <59221853+Mister-718@users.noreply.github.com>
2022-06-09 19:55:18 +0600

    several indicators requested at a time

--- script.js
+++ script.js
@@ -2,13 +2,24 @@
 const axios = require("axios");
 
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHVlIjoiNjJhMWY1NDE2YzI4YjU1Y2Q1ZmQ3ZGJmIiwiaWF0IjoxNjU0NzgxMjQ5LCJleHAiOjMzMTU5MjQ1MjQ5fQ.N2LkEIWc5zu6bpQgN5uBmMKg5zhSoeVbrUC7nnRKkug"
-const options = {
+const rsi = {
   method: 'GET',
   url: `https://api.taapi.io/rsi?secret=${key}&exchange=binance&symbol=BTC/USDT&interval=1h`
 };
+const macd = {
+    method: 'GET',
+    url: `https://api.taapi.io/macd?secret=${key}&exchange=binance&symbol=BTC/USDT&interval=1h`
+}
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
+
+axios.request(rsi).then(function (response) {
+	console.log(response.data.value);
+}).catch(function (error) {
+	console.error(error);
+});
+
+axios.request(macd).then(function (response) {
+	console.log(response.data.value);
 }).catch(function (error) {
 	console.error(error);
 });
